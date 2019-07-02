eeef6c3fc20a8b4fde7a743fba9f905668eb1d09
Sooze16 <slkpes2002@yahoo.com>
2019-07-01 22:31:53 -0500

    added arguments

--- liri.js
+++ liri.js
@@ -2,9 +2,88 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
+var spotify = new Spotify(keys.spotify);
+
 var axios = require("axios");
 
-axios.get("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy").then(
-    function(response) {
-        console.log("The movie's rating is: " + response.data.imdbRating);
-    });
+var fs = require("fs");
+
+var moment = require("moment");
+
+var command = process.argv[2];
+
+
+switch (command) {
+    case "concert-this":
+        concertThis();
+        break;
+    case "spotify-this-song":
+        spotifyThis();
+        break;
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "do-what-it-says":
+        doWhatIS();
+        break;
+};
+
+function command()
+
+if (command === movieThis)
+    axios.get("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy").then(
+        function(response) {
+            console.log("Title of the movie: " + response.data.Title);
+            console.log("Year the movie came out: " + response.data.Year);
+            console.log("IMBD Rating of the movie: " + response.data.imdbRating);
+            console.log("Rotton Tomatoes Rating of the movie: " + response.data.Ratings[1].Value);
+            console.log("Country where the moivie was produced: " + response.data.Country);
+            console.log("Language of the move: " + response.data.Language);
+            console.log("Plot of the movie: " + response.data.Plot);
+            console.log("Actors in the movie: " + response.data.Actors);
+
+        })
+
+
+
+.catch(function(error) {
+    if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+    } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+    } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+    }
+    console.log(error.config);
+})
+
+else if (command === "concertThis") {
+
+    axios.get("https://rest.bandsintown.com/artists/" + searchItem + "/events?app_id=codingbootcamp").then(
+        function(response) {
+
+        })
+
+
+
+    else if (command === "do-what-it-says") {
+        fs.readFile("random.text, "
+            utf8 ", function(error, random) {
+
+            if (error) {
+                return console.log(random)
+            }
+
+        })
+}
