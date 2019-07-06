2e72c75d36b48a783f27d399845b8b7f8f2e801e
Sooze16 <slkpes2002@yahoo.com>
2019-07-05 21:50:47 -0500

    read me

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# liri-node-app
+# LIRI Bot Assignment 10
+
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+
+## LIRI Bot Functions
+
+- movie-this
+- concert-this
+- spotify-this-song
+- do-what it says
+
+## What Each Command Should Do:
+
+#### node liri.js movie-this '<movie name here>'
+* This will output the following information to your terminal/bash window:
+
+   * Title of the movie.
+   * Year the movie came out.
+   * IMDB Rating of the movie.
+   * Rotten Tomatoes Rating of the movie.
+   * Country where the movie was produced.
+   * Language of the movie.
+   * Plot of the movie.
+   * Actors in the movie.
+
+
+If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+If you haven't watched "Mr. Nobody," then you should: It's on Netflix!
+
+#### node liri.js concert-this <artist/band name here>
+* This will search the Bands in Town Artist Events API for an artist and render the following information about each event to the terminal:
+
+
+* Name of the venue
+* Venue location
+* Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+#### node liri.js spotify-this-song '<song name here>'
+
+* This will show the following information about the song in your terminal/bash window
+
+
+* Artist(s)
+* The song's name
+* A preview link of the song from Spotify
+* The album with that song title
+
+If no song is provided then your program will default to "The Sign" by Ace of Base.
+
+
+#### node liri.js do-what-it-says
+
+* Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+
+
+It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+Edit the text in random.txt to test out the feature for movie-this and concert-this.
+
+### Technologies used in this project
+
+- javascript
+- jQuery
+- Node JS
+- moment.js
+- fs
+- axios
+- .gitignore
+- node-spotify-api
+- OMBD api
+- bandsintown API
--- liri.js
+++ liri.js
@@ -13,22 +13,29 @@ var moment = require("moment");
 var command = process.argv[2];
 var searchItem = process.argv.slice(3).join(" ");
 
+execute(command, searchItem)
 
-switch (command) {
-    case "concert-this":
-        concertThis(searchItem);
-        break;
-    case "spotify-this-song":
-        spotifyThis(searchItem);
-        break;
-    case "movie-this":
-        movieThis(searchItem);
-        break;
+function execute(command, searchItem) {
+    switch (command) {
+        case "concert-this":
+            concertThis(searchItem);
+            break;
+        case "spotify-this-song":
+            spotifyThis(searchItem);
+            break;
+        case "movie-this":
+            movieThis(searchItem);
+            break;
 
-    case "do-what-it-says":
-        doWhatItSays();
-        break;
-};
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+
+        default:
+            console.log("I do not understand.");
+
+    };
+}
 
 
 
@@ -44,23 +51,38 @@ function movieThis(searchItem) {
             console.log("Plot of the movie: " + response.data.Plot);
             console.log("Actors in the movie: " + response.data.Actors);
             console.log("----------------------------------------------------------------" + "\n")
+            var text =
 
+                fs.appendFile("log.txt", JSON.stringify(response.data).split(",").join("\n") + "\n" + "----------" + "\n", 'utf8', function(err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log("Information has been added to the log.");
+                });
         })
 
     .catch(function(error) {
         axios.get("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy").then(
             function(response) {
-                console.log("Title of the movie: " + response.data.Title);
+                console.log("Hmmm...that does not exist. May I suggest: " + response.data.Title);
                 console.log("Year the movie came out: " + response.data.Year);
                 console.log("IMBD Rating of the movie: " + response.data.imdbRating);
                 console.log("Rotton Tomatoes Rating of the movie: " + response.data.Ratings[1].Value);
-                console.log("Country where the moivie was produced: " + response.data.Country);
-                console.log("Language of the move: " + response.data.Language);
+                console.log("Country where the movie was produced: " + response.data.Country);
+                console.log("Language of the movie: " + response.data.Language);
                 console.log("Plot of the movie: " + response.data.Plot);
                 console.log("Actors in the movie: " + response.data.Actors);
                 console.log("----------------------------------------------------------------" + "\n")
 
+                fs.appendFile("log.txt", JSON.stringify(response.data), function(err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log("Information has been added to the log.");
+                });
+
             })
+
     })
 }
 
@@ -81,7 +103,7 @@ function concertThis(searchItem) {
         .catch(function(error) {
             axios.get("https://rest.bandsintown.com/artists/rolling+stones/events?app_id=codingbootcamp").then(
                 function(response) {
-                    console.log("Artist searched: rolling stones", )
+                    console.log("Hmmm, I don't see that,  May I suggest The Rolling Stones.", )
                     console.log("Artist will be playing next at: " + response.data[0].venue.name);
                     console.log("The venue is located at: " + response.data[0].venue.city + ", " + response.data[0].venue.country);
                     console.log("All artists being featured at this event: " + response.data[0].lineup)
@@ -90,8 +112,17 @@ function concertThis(searchItem) {
                     console.log("Tickets for this event is currently: " + response.data[0].offers[0].status);
                     console.log("-------------------------------------------------------------------" + "\n")
 
+                    fs.appendFile("log.txt", JSON.stringify(response.data).split(",").join("\n") + "\n" + "----------" + "\n", 'utf8', function(err) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        console.log("Information has been added to the log.");
+                    });
+
                 })
-        });
+        })
+
+
 }
 
 function spotifyThis(searchItem) {
@@ -100,9 +131,13 @@ function spotifyThis(searchItem) {
         if (err) {
             console.log(`!!!Error occured: ${err})`);
             return;
+            ///Return "I saw the Sign by Ace of Base"
         }
         // console.log(data);
         var songs = data.tracks.items;
+        if (songs.length === 0) {
+            spotifyThis("I saw the Sign");
+        }
         for (var i = 0; i < songs.length; i++) {
             // console.log(songs[0]);
             console.log("Artist(s): " + songs[i].artists[0].name);
@@ -111,5 +146,27 @@ function spotifyThis(searchItem) {
             console.log("Album: " + songs[i].album.name);
             console.log("---------------------------------------" + "\n")
         }
+
+        fs.appendFile("log.txt", JSON.stringify(songs).split(",").join("\n") + "\n" + "----------" + "\n", 'utf8', function(err) {
+            if (err) {
+                console.log(err);
+            }
+            console.log("Information has been added to the log.");
+        });
     })
+}
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log(data);
+
+        var dataArr = data.split(",");
+        searchItem = dataArr[1];
+        execute(dataArr[0], dataArr[1])
+
+    });
 }
